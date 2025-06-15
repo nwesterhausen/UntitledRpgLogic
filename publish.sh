88d58b43b5a5694f914d2ca5837d65d7e79023b4
Nick Westerhausen <nwesterhausen@gmail.com>
2025-06-15 11:19:04 -0400

    fix publish script

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ cd UntitledRpgLogic || exit 1
 dotnet pack --configuration Release
 
 # Find the most recently modified UntitledRpg.Logic*.nupkg file (excluding .snupkg)
-NUPKG=$(find ./bin/Release -type f -name "UntitledRpg.Logic*.nupkg" ! -name "*.snupkg" -print0 | xargs -0 ls -t | head -n 1)
+NUPKG=$(find ./bin/Release -type f -name "UntitledRpg.Logic*.nupkg" ! -name "*.symbols.nupkg" ! -name "*.snupkg" -print0 | xargs -0 ls -t | head -n 1)
 
 if [[ -z "$NUPKG" ]]; then
   echo "No UntitledRpg.Logic .nupkg file found!"
