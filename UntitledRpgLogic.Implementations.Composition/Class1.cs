d79c11086b20edc1f417607865aa602e51208c43
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2025-07-03 10:29:04 -0400

    refactor: skeleton project creation

---
+++ UntitledRpgLogic.Implementations.Composition/Class1.cs
@@ -0,0 +1,6 @@
+namespace UntitledRpgLogic.Implementations.Composition;
+
+public class Class1
+{
+    //Todo: the implementations in this project likely should migrate into default interface methods/etc.
+}
---
+++ UntitledRpgLogic.Services/CultureService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UntitledRpgLogic.Services
+{
+    internal class CultureService
+    {
+    }
+}
