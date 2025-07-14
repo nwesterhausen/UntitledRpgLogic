467d12ae48ee8c800c1d64d7879c0e1f27d2532a
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2025-07-14 10:18:12 -0400

    add serilog

--- UntitledRpgLogic.DevTool/Program.cs
+++ UntitledRpgLogic.DevTool/Program.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using UntitledRpgLogic.DevTool;
 using UntitledRpgLogic.Infrastructure.Configuration;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.BrowserConsole()
+    .CreateLogger();
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
