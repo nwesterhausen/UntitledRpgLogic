fdd2ee39f4fe786a3881d5578afd6fbff5f3e136
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2025-07-14 13:10:34 -0400

    provide serilog as logger

--- UntitledRpgLogic.DevTool/Program.cs
+++ UntitledRpgLogic.DevTool/Program.cs
@@ -5,19 +5,35 @@ using UntitledRpgLogic.DevTool.Services;
 using UntitledRpgLogic.DevTool.Services.Contracts;
 using UntitledRpgLogic.Infrastructure.Configuration;
 using Serilog;
+using Serilog.Extensions.Logging;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.BrowserConsole()
     .CreateLogger();
 
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+try
+{
+    WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddConfigurationInfrastructure();
-builder.Services.AddSingleton<IConfigStore, ConfigStoreService>();
+    builder.Services.AddConfigurationInfrastructure();
+    builder.Services.AddSingleton<IConfigStore, ConfigStoreService>();
 
-await builder.Build().RunAsync();
+    // Replace Microsoft logger for Serilog
+    builder.Logging.ClearProviders();
+    builder.Logging.AddProvider(new SerilogLoggerProvider(Log.Logger));
+
+
+    await builder.Build().RunAsync();
+} catch (Exception ex)
+{
+    Log.Fatal(ex, "An error occurred during application startup");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
