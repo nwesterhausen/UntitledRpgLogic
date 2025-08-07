01ba957936e4878bfc0cec2b6c439cfbd28ffa15
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2025-08-07 09:48:27 -0400

    address more compiler warnings

--- src/UntitledRpgLogic.Core/Enums/TargetingType.cs
+++ src/UntitledRpgLogic.Core/Enums/TargetingType.cs
@@ -13,27 +13,27 @@ public enum TargetingType
 	/// <summary>
 	///     Targets a single entity.
 	/// </summary>
-#pragma warning disable CA1720
-	Single = 1,
-#pragma warning restore CA1720
-
-	/// <summary>
-	///     Targets an area, affecting all entities within that area.
-	/// </summary>
-	AreaOfEffect = 2,
-
-	/// <summary>
-	///     Requires physical touch to apply the effect.
-	/// </summary>
-	Touch = 3,
-
-	/// <summary>
-	///     Targets the caster or user of the effect.
-	/// </summary>
-	Self = 4,
+	SingleTarget = 1,
 
 	/// <summary>
 	///		Targets multiple specific entities, which may include allies or enemies.
 	/// </summary>
-	Multiple = 5
+	MultipleTarget = 2,
+
+	/// <summary>
+	///     Targets an area, affecting all entities within that area.
+	/// </summary>
+	AreaOfEffect = 3,
+
+	/// <summary>
+	///     Targets the caster or user of the effect.
+	/// </summary>
+	/// <remarks>This implies a single target.</remarks>
+	Self = 4,
+
+	/// <summary>
+	///     Requires physical touch to apply the effect.
+	/// </summary>
+	/// <remarks>This implies a single target.</remarks>
+	Touch = 5,
 }
